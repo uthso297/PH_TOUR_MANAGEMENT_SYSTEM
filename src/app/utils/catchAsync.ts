83c3e5e1808d9e00eae23db1b23eb8cf09a20768
uthso297 <uthso119464@gmail.com>
2025-07-14 05:22:28 +0600

    solved repetition of try-catch and send response and created all users get route

---
+++ src/app/utils/catchAsync.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+import { NextFunction, Request, Response } from "express";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>
+
+export const catchAsync = (fn: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err: any) => {
+        console.log(err);
+        next(err)
+    })
+}
