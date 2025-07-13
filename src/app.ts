e814609bcbcf59ad8f173ac04ef5664c7aaab6ea
uthso297 <uthso119464@gmail.com>
2025-07-13 22:12:05 +0600

    added server error handler

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express'
+
+
+const app = express()
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Welcome to Tour Management System Backend Server☺️"
+    })
+})
+
+export default app
