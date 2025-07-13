19438886582b5ddb17b233786f850671b6b3abac
uthso297 <uthso119464@gmail.com>
2025-07-13 23:28:41 +0600

    project setup completed

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,19 @@
 import { Server } from 'http'
 import mongoose from 'mongoose'
 import app from './app'
+import { envVars } from './app/config/env'
 
 let server: Server
 
 
 const startServer = async () => {
     try {
-        await mongoose.connect('mongodb+srv://uthsoDB:UthsoMef3647@cluster0.g3mp55c.mongodb.net/tour-management-backend?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect(envVars.DB_URL)
 
         console.log('Connectet to mongodb😍');
 
-        server = app.listen(5000, () => {
-            console.log('Server is listening on port 5000😊');
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server is listening on port ${envVars.PORT} 😊`);
         })
     } catch (error) {
         console.log(error);
