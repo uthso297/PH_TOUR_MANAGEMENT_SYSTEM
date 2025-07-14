471629f76bc0e26034d983dbd0250cf1379ef942
uthso297 <uthso119464@gmail.com>
2025-07-14 18:35:14 +0600

    created zod schema and validate function

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
 const router = Router()
-router.post("/register", UserController.createUser)
+router.post("/register", validateRequest(createUserZodSchema),UserController.createUser)
 router.get("/all-users", UserController.getAllUsers)
 
 export const UserRoutes = router
