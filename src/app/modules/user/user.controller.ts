9c8af60c52a11fcc9a684885c240076d7fa70fa3
uthso297 <uthso119464@gmail.com>
2025-07-16 01:55:56 +0600

    completed part-2

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -5,9 +5,9 @@ import httpStatus from 'http-status-codes'
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
-import { verifyToken } from "../../utils/jwt";
-import { JwtPayload } from "jsonwebtoken";
-import { envVars } from "../../config/env";
+// import { verifyToken } from "../../utils/jwt";
+// import { JwtPayload } from "jsonwebtoken";
+// import { envVars } from "../../config/env";
 
 // const createUer = async (req: Request, res: Response, next: NextFunction) => {
 //     try {
@@ -54,8 +54,9 @@ const createUser = catchAsync(async (req: Request, res: Response, next: NextFunc
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const updateUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.params.id;
-    const token = req.headers.authorization
-    const verifiedToken = verifyToken(token as string, envVars.JWT_ACCESS_SECRET) as JwtPayload
+    // const token = req.headers.authorization
+    // const verifiedToken = verifyToken(token as string, envVars.JWT_ACCESS_SECRET) as JwtPayload
+    const verifiedToken = req.user
     const payload = req.body;
     const user = await UserServices.updateUser(userId, payload, verifiedToken)
 
