762dc5e0c2ed7f5c825fcac196e2dc9cba19af0a
uthso297 <uthso119464@gmail.com>
2025-07-31 21:30:03 +0600

    completed forget and resetpassword with nodemailer

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -11,6 +11,8 @@ router.post("/refresh-token", AuthControllers.getNewAccessToken)
 router.post('/logout', AuthControllers.logout)
 router.post('/change-password', checkAuth(...Object.values(Role)), AuthControllers.changePassword)
 router.post("/set-password", checkAuth(...Object.values(Role)), AuthControllers.setPassword)
+router.post("/forgot-password", AuthControllers.forgotPassword)
+router.post("/reset-password", checkAuth(...Object.values(Role)), AuthControllers.resetPassword)
 router.get("/google", async (req: Request, res: Response, next: NextFunction) => {
     const redirect = req.query.redirect || '/'
     passport.authenticate("google", { scope: ["profile", "email"], prompt: 'select_account', state: redirect as string })(req, res, next)
