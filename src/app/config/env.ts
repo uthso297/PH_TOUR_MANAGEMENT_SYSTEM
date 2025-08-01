89e4f24f6d73eb1c36696fee9d51e14b6cc1a79c
uthso297 <uthso119464@gmail.com>
2025-08-01 07:24:02 +0600

    initiated redis connection

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -45,6 +45,11 @@ interface EnvConfig {
         SMTP_FROM: string;
     };
 
+    REDIS_HOST: string;
+    REDIS_PORT: string;
+    REDIS_USERNAME: string;
+    REDIS_PASSWORD: string;
+
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -61,7 +66,11 @@ const loadEnvVariables = (): EnvConfig => {
         "SMTP_PORT",
         "SMTP_HOST",
         "SMTP_USER",
-        "SMTP_FROM"
+        "SMTP_FROM",
+        "REDIS_HOST",
+        "REDIS_PORT",
+        "REDIS_USERNAME",
+        "REDIS_PASSWORD"
     ];
 
     requiredEnvVariables.forEach(key => {
@@ -111,7 +120,10 @@ const loadEnvVariables = (): EnvConfig => {
             SMTP_HOST: process.env.SMTP_HOST as string,
             SMTP_FROM: process.env.SMTP_FROM as string,
         },
-
+        REDIS_HOST: process.env.REDIS_HOST as string,
+        REDIS_PORT: process.env.REDIS_PORT as string,
+        REDIS_USERNAME: process.env.REDIS_USERNAME as string,
+        REDIS_PASSWORD: process.env.REDIS_PASSWORD as string,
 
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import app from './app'
 import { envVars } from './app/config/env'
 import seedSuperAdmin from './app/utils/seedSuperAdmin'
+import { connectRedis } from './app/config/redis.config'
 
 let server: Server
 
@@ -25,6 +26,7 @@ const startServer = async () => {
 
 // iife function
 (async () => {
+    await connectRedis();
     await startServer();
     await seedSuperAdmin();
 })()
